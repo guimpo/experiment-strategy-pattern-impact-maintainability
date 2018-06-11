965d3d72cad1790ede8fc76095cece8006c38e64
Paulo Nakaima <nakaima.prk@gmail.com>
2018-06-11 05:46:21 -0300

    Adicionado AbstractSalvarBuscarDaoFactory

---
+++ src/main/java/com/mycompany/strategyexperiment/dao/experiment/AbstractSalvarBuscarDaoFactory.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.strategyexperiment.dao.experiment;
+
+public abstract class AbstractSalvarBuscarDaoFactory {
+    
+    public abstract AbstractSalvarBuscarDAO getSalvarBuscarDao();
+    
+}
